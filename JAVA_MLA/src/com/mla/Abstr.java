4dc3adc7e47fed212440e4f49cd5c544be20888a
omsri674 <omsri0088@gmail.com>
2025-02-28 22:06:01 +0530

    file added

---
+++ JAVA_MLA/src/com/mla/Abstr.java
@@ -0,0 +1,19 @@
+package JAVA_MLA.src.com.mla;
+
+import java.util.*;
+public class Abstr
+{
+    public static void  main(String[] sri)
+    {
+        int[] arr = {56, 23, 45, 67, 78, 98, 76, 54, 32, 21, 10};
+        Arrays.sort(arr);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        //even on left and odd on right side
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int l=0, r=num.length;
+
+    }
+
+}
