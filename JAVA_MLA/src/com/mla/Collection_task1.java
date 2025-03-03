d2628687f03cf1513334f548455e0d868d52b3cf
omsri674 <omsri0088@GMAIL.COM>
2025-03-03 17:18:58 +0530

    collection 2.0

--- JAVA_MLA/src/com/mla/Collection_task1.java
+++ JAVA_MLA/src/com/mla/Collection_task1.java
@@ -9,8 +9,11 @@ class Employee
 id name age gender YOJ salary
 
 
-1. iterate using stream API and print starts with a specific character
+1. ---iterate using stream API and print starts with a specific character
 2. who joined after 2020 using stream API 
 3. (2) and in sorted manner and store in new list
 
+
+4. add the duplicates and count the duplicates
+and print only unique data
 */
